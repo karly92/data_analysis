388d964280585f45503d0ec42f18391c2d79e382
karly92 <karla.p.s.c10@gmail.com>
2014-12-18 22:54:50 -0600

    primera modificacion de los paneles

--- ui.R
+++ ui.R
@@ -7,12 +7,18 @@
 
 library(shiny)
 shinyUI(pageWithSidebar(
-  headerPanel("Data science FTW!"),
+  headerPanel("Illustrating markup"),
   sidebarPanel(
-    h3('Sidebar text')
+    h3('Sidebar panel'),
+    h1('H1 text'),
+    h2('H2 Text'),
+    h3('H3 Text'),
+    h4('H4 Text')
   ),
   mainPanel(
-    h3('Main Panel text')
+    h3('Main Panel text'),
+    code('some code'),
+    p('some ordinary text')
     )
   ))
 
